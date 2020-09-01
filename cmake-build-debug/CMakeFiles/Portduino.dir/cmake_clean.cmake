baffacc022cb8bfaf142d7546dca79c68387768f
geeksville <kevinh@geeksville.com>
2020-09-01 12:59:28 -0700

    add temporary cmake/clion build script

---
+++ cmake-build-debug/CMakeFiles/Portduino.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Portduino.dir/cores/arduino/api/Common.cpp.o"
+  "CMakeFiles/Portduino.dir/cores/arduino/api/IPAddress.cpp.o"
+  "CMakeFiles/Portduino.dir/cores/arduino/api/Print.cpp.o"
+  "CMakeFiles/Portduino.dir/cores/arduino/api/Stream.cpp.o"
+  "CMakeFiles/Portduino.dir/cores/arduino/api/String.cpp.o"
+  "CMakeFiles/Portduino.dir/src/main.cpp.o"
+  "Portduino"
+  "Portduino.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Portduino.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, const char **argv) {
+  printf("hello world\n");
+  return 0;
+}

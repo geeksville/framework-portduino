ac5b02e259ca656fa6ebea9ba8f6f44252ac7cc6
geeksville <kevinh@geeksville.com>
2020-09-06 15:35:46 -0700

    move FS into core

--- cores/portduino/main.cpp
+++ cores/portduino/main.cpp
@@ -1,19 +1,26 @@
-#include <stdio.h>
 #include "Arduino.h"
+#include "PortduinoFS.h"
+#include <stdio.h>
 
-/** # msecs to sleep each loop invocation.  FIXME - make this controlable via config file or command
- * line flags.
+/** # msecs to sleep each loop invocation.  FIXME - make this controlable via
+ * config file or command line flags.
  */
 static long loopDelay = 100;
 
 int main(int argc, const char **argv) {
   printf("Portduino is starting\n");
-  setup();
-  while(true) {
-      loop();
 
-      // Even if the Arduino code doesn't want to sleep, ensure we don't  burn too much CPU
-      delay(loopDelay);
+  String fsroot("/tmp/portduinofs");
+  mkdir(fsroot, 0);
+  PortduinoFS.mountpoint(fsroot);
+
+  setup();
+  while (true) {
+    loop();
+
+    // Even if the Arduino code doesn't want to sleep, ensure we don't  burn too
+    // much CPU
+    delay(loopDelay);
   }
   return 0;
 }
